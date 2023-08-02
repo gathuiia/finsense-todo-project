6a6b7998f5b4f23d20d5d3ef192345dd5fdbb753
Ian Gathui <ian.gathui@student.moringaschool.com>
2023-08-02 13:28:49 +0300

    added tasklist resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'task_lists/index'
+  get 'task_lists/show'
+  get 'task_lists/create'
+  get 'task_lists/update'
+  get 'task_lists/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
