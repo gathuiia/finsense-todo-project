b5c30a4abbd87f0596e106ceeb14c2cd4826fd54
Ian Gathui <ian.gathui@student.moringaschool.com>
2023-08-02 14:35:12 +0300

    basic CRUD added

--- app/controllers/task_lists_controller.rb
+++ app/controllers/task_lists_controller.rb
@@ -1,16 +1,39 @@
 class TaskListsController < ApplicationController
+  
   def index
+    task_lists = TaskList.all
+    render json: task_lists, status: :ok
   end
 
   def show
+    task_list = find_task_list
+    render json: task_list, status: :ok
   end
 
   def create
+    task_list = TaskList.create!(task_list_params)
+    render json: task_list, status: :created
   end
 
   def update
+    task_list = find_task_list
+    task_list.update!(task_list_params)
+    render json: task_list, status: :ok
   end
 
   def destroy
+    task_list = find_task_list
+    task_list.destroy
+    head :no_content
+  end
+
+  private
+
+  def task_list_params
+    params.permit(:description)
+  end
+
+  def find_task_list
+    TaskList.find(params[:id])
   end
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,16 +1,39 @@
 class TasksController < ApplicationController
+
   def index
+    tasks = Task.all
+    render json: tasks, status: :ok
   end
 
   def show
+    task = find_task
+    render json: task, status: :ok
   end
 
   def create
+    task = Task.create!(task_params)
+    render json: task, status: :created
   end
 
   def update
+    task = find_task
+    task.update!(task_params)
+    render json: task, status: :ok
   end
 
   def destroy
+    task = find_task
+    task.destroy
+    head :no_content
+  end
+
+  private
+
+  def task_params
+    params.permit(:description)
+  end
+
+  def find_task
+    Task.find(params[:id])
   end
 end
