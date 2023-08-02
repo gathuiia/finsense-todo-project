c4f03bf27e167a4521511288ef6e45cad62eb5fc
Ian Gathui <ian.gathui@student.moringaschool.com>
2023-08-02 14:09:39 +0300

    simplified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
-  get 'tasks/show'
-  get 'tasks/create'
-  get 'tasks/update'
-  get 'tasks/destroy'
-  get 'task_lists/index'
-  get 'task_lists/show'
-  get 'task_lists/create'
-  get 'task_lists/update'
-  get 'task_lists/destroy'
+  # get 'tasks/index'
+  # get 'tasks/show'
+  # get 'tasks/create'
+  # get 'tasks/update'
+  # get 'tasks/destroy'
+  # get 'task_lists/index'
+  # get 'task_lists/show'
+  # get 'task_lists/create'
+  # get 'task_lists/update'
+  # get 'task_lists/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  resources :task_lists
+  resources :tasks
 
   # Defines the root path route ("/")
   # root "articles#index"
