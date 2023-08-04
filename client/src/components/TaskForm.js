641ae34436ce9fc4a4338672ef53abd269d07058
Ian Gathui <ian.gathui@student.moringaschool.com>
2023-08-04 09:33:11 +0300

    forms completed not collecting entries

--- client/src/components/TaskForm.js
+++ client/src/components/TaskForm.js
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-function TaskForm() {
-  return (
-    <div>
-        <form className='TaskForm'>
-        <input type='text' className='task-input' placeholder='New Task'></input>
-        <button type='submit' className='task-btn'>Add Task</button>
+function TaskForm({addTask}) {
+    const [value, setValue] = useState('');
+
+    const handleSubmit = function(e) {
+
+        e.preventDefault();
+        if (value){
+            addTask(value);
+            console.log(value)
+            setValue('');
+        }
+
+    };
+
+    return (
+        <form onSubmit={handleSubmit} className='TaskForm'>
+            <input type='text' value={value} onChange={(e) => setValue(e.target.value)} className='task-input' placeholder='New Task'></input>
+            <button type='submit' className='task-btn'>Add Task</button>
         </form>
-    </div>
     
-  )
+    )
 }
 
 export default TaskForm
--- client/src/components/TaskListForm.js
+++ client/src/components/TaskListForm.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-function TaskListForm() {
+function TaskListForm(addList) {
+    const [value, setValue] = useState('');
+
+    const handleSubmit = function(e) {
+
+        e.preventDefault();
+        if (value){
+            addList(value);
+            console.log(value)
+            setValue('');
+        }
+
+    };
     
     return (
-        <form className='ListForm'>
-          <input type='text' className='task-input' placeholder='New Task List'/>
-          <button type='submit' className='task-btn'>Add List</button>
+        <form onSubmit={handleSubmit} className='ListForm'>
+            <input type='text' value={value} onChange={(e) => setValue(e.target.value)} className='task-input' placeholder='New Task List'/>
+            <button type='submit' className='task-btn'>Add List</button>
         </form>
     )
 }
